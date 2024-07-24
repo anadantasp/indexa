1b5f2d86db2e3b4eab6d47e2f94b116a7266a7b2
anadantasp <caroldantasp@gmail.com>
2024-07-24 10:14:36 -0300

    Implementando o input e NgModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { ContainerComponent } from './componentes/container/container.component'
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
 import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
 
 interface Contato {
   id: number
@@ -13,6 +14,7 @@ interface Contato {
 }
 
 import agenda from './agenda.json'
+
 
 @Component({
   selector: 'app-root',
@@ -23,7 +25,8 @@ import agenda from './agenda.json'
     ContainerComponent, 
     CabecalhoComponent, 
     SeparadorComponent,
-    ContatoComponent
+    ContatoComponent,
+    FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -32,9 +35,24 @@ export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
   contatos: Contato[] = agenda
 
+  filtroPorTexto: string = ''
+
+  private removerAcentos(texto: string): string{
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatoPorTexto(): Contato[]{
+    if(!this.filtroPorTexto){
+      return this.contatos
+    }
+    return this.contatos.filter(contato => {
+      return this.removerAcentos(contato.nome).toLowerCase().includes(this.removerAcentos(this.filtroPorTexto).toLowerCase())
+    })
+  }
+
   filtrarContatosPorLetraInicial(letra: string) : Contato[]{
-    return this.contatos.filter( contato => {
-      return contato.nome.toLowerCase().startsWith(letra)
+    return this.filtrarContatoPorTexto().filter( contato => {
+      return this.removerAcentos(contato.nome).toLowerCase().startsWith(this.removerAcentos(letra).toLowerCase())
     })
   }
 }
